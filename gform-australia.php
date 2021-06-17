f8e95713ec65fd87a46fe50050279f7a95f8be79
Mathew Callaghan <mathewcallaghan@gmail.com>
2021-06-17 10:16:53 +1000

    Initial commit

---
+++ gform-australia.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @link              https://github.com/mathewcallaghan/gform-australia
+ * @since             1.0.0
+ * @package           Gform_Australia
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Gform Australia
+ * Plugin URI:        https://github.com/mathewcallaghan/gform-australia
+ * Description:       This plugin adds an Australian address type.
+ * Version:           1.0.0
+ * Author:            Mathew Callaghan
+ * Author URI:        https://mathew.callaghan.xyz/
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ * GitHub Plugin URI: mathewcallaghan/gform-australia
+ * Text Domain:       gform-australia
+ * Domain Path:       /languages
+ */
+
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+function australian_address_type( $address_types ) {
+
+    $address_types['australia'] = array(
+        'label'       => 'Australian',
+        'country'     => 'Australia',
+        'zip_label'   => 'Postcode',
+        'state_label' => 'State',
+        'states'      => array(
+            'ACT' => 'Australian Capital Territory',
+            'NSW' => 'New South Wales',
+            'NT'  => 'Northern Territory',
+            'QLD' => 'Queensland',
+            'SA'  => 'South Australia',
+            'TAS' => 'Tasmania',
+            'VIC' => 'Victoria',
+            'WA'  => 'Western Australia',
+        )
+    );
+ 
+    return $address_types;
+}
+
+add_filter( 'gform_address_types', 'australian_address_type' );
---
+++ uninstall.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ *
+ * @link       https://github.com/mathewcallaghan/gform-australia
+ * @since      1.0.0
+ *
+ * @package    Gform_Australia
+ */
+
+// if uninstall.php is not called by WordPress, die
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    die;
+}
